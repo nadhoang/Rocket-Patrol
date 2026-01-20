34eb8e18732717d89f01d00821b8506bf437e396
Nathan Hoang <nadhoang@ucsc.edu>
2026-01-19 22:55:22 -0800

    finished game product yaaay :confetti:

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -2,9 +2,9 @@
 class Spaceship extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
         super(scene, x, y, texture, frame)
-        scene.add.existing(this)    // add to existing scene
-        this.points = pointValue    // store pointValue
-        this.moveSpeed = 3          // spaceship speed in pixels/frame
+        scene.add.existing(this)                        // add to existing scene
+        this.points = pointValue                        // store pointValue
+        this.moveSpeed = game.settings.spaceshipSpeed   // spaceship speed in pixels/frame
     }
 
     update() {
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -36,7 +36,7 @@ class Play extends Phaser.Scene {
             align: 'right',
             padding: {
                 top: 5,
-                bottom:5,
+                bottom: 5,
             },
             fixedWidth: 100
         }
@@ -47,9 +47,9 @@ class Play extends Phaser.Scene {
 
         // 60-second play clock
         scoreConfig.fixedWidth = 0
-        this.clock = this.time.delayedCall(60000, () => {
+        this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5)
-            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5)
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or <- for Menu', scoreConfig).setOrigin(0.5)
             this.gameOver = true
         }, null, this)
     }
@@ -58,6 +58,9 @@ class Play extends Phaser.Scene {
         // check key input for restart
         if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyRESET)) {
             this.scene.restart()
+        }
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            this.scene.start("menuScene")
         }
 
         this.starfield.tilePositionX -= 4
